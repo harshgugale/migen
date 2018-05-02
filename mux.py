8ef17439b4a044660e3752c5681d4bb8237bc2ad
harshgugale <hashok.gugale.ece14@iitbhu.ac.in>
2018-05-02 16:40:48 +0530

    First commit

---
+++ mux.py
@@ -0,0 +1,21 @@
+from migen import *
+
+class mux(Module):
+		
+	def __init__ (self,selb):
+		self.out = out = Signal()
+		self.sel = sel = Signal(selb)
+		self.inp = inp = Signal(2**selb)
+		
+		for j in range(2**selb):
+			self.comb += If(sel == j,out.eq(inp[sel]))
+			
+	
+def testbench(dut):
+	yield dut.inp.eq(5)
+	for i in range(8):
+		yield dut.sel.eq(i)
+		print("Sel {} out {}".format(i,(yield dut.out)))
+
+dut = mux(3)
+run_simulation(dut,tesbench(dut))
