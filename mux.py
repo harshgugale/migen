175496c1f9f02d8017779ec3830e93dcc2ba52da
harshgugale <hashok.gugale.ece14@iitbhu.ac.in>
2018-05-02 19:01:49 +0530

    changes

--- mux.py
+++ mux.py
@@ -1,4 +1,5 @@
 from migen import *
+from migen.fhdl import verilog
 
 class mux(Module):
 		
@@ -8,14 +9,15 @@ class mux(Module):
 		self.inp = inp = Signal(2**selb)
 		
 		for j in range(2**selb):
-			self.comb += If(sel == j,out.eq(inp[selb]))
-			
+			self.comb += If(sel == j,out.eq(inp[j]))	
 	
-def testbench(dut):
-	yield dut.inp.eq(5)
-	for i in range(8):
-		yield dut.sel.eq(i)
-		print("Sel {} out {}".format(i,(yield dut.out)))
-
+def testbench(dut,selb):
+ 	yield dut.inp.eq(0b00000001)
+ 	for i in range(2**selb):
+ 		yield dut.sel.eq(i)
+ 		print("Sel {} out {}".format(i,(yield dut.out)))
+ 		yield
+		
 dut = mux(3)
-run_simulation(dut,testbench(dut))
+#run_simulation(dut,testbench(dut,3))
+print(verilog.convert(dut,{dut.inp,dut.out,dut.sel}))
